9aa4ec5e1c6cf4a07a91e6fddbe30de3ab5a1692
Gabriel <gabebigelow@gmail.com>
2022-03-17 14:59:43 -0400

    create get requests for random quote and specific authors

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const e = require('express');
 const express = require('express');
 const app = express();
 
@@ -9,4 +10,23 @@ const PORT = process.env.PORT || 4001;
 app.use(express.static('public'));
 
 app.listen(PORT);
+
+app.get('/api/quotes/random', (req, res, next) => {
+    const randomQuote = getRandomElement(quotes);
+    res.send(randomQuote);
+});
+
+app.get('/api/quotes', (req, res, next) => {
+    const reqPerson = req.query.person;
+    const quotesBy = quotes.filter(quote => quote.person === reqPerson);
+    const quotesOnly = { quotes: []};
+    
+    if (req.query.person){
+        quotesBy.forEach(element => quotesOnly.quotes.push(element.quote));
+        res.send(quotesOnly);
+    } else {
+        quotes.forEach(element => quotesOnly.quotes.push(element.quote));
+        res.send(quotesOnly);
+    }    
+});
 
